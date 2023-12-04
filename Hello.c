485e6c395b9ad9b38da8711d48b5102aa5d2a4af
surajatresh300 <surajatresh300@gmail.com>
2023-12-04 13:01:11 +0530

    Added 3 files into the repository

--- Hello.c
+++ Hello.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 int main()
 {
-  printf("Hello world")
+  printf("Hello world");
+  printf("Hello from the other sidee");
 }
--- sample.py
+++ sample.py
@@ -1,1 +1,2 @@
 print("This is a sample python program")
+print("This is a sample python code from vscode")
